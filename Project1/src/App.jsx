1b2f9a6a85c6512b710e2c82a16fde8ede9defab
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-08-31 08:34:05 +0530

    Project1 created

---
+++ Project1/src/App.jsx
@@ -0,0 +1,12 @@
+import Person from "./Person";
+const App = () => {
+  return (
+    <>
+      <h1> Person Data </h1>
+      <Person name="Akash Singh" age={20} />
+      <Person name="Abhishek" age={25} />
+    </>
+  )
+}
+
+export default App;
